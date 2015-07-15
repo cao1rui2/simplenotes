ab75fb07584b24350a0367c83ac9b8cfc7b45fa2
cao1rui2 <cao1rui2@163.com>
2015-07-15 14:40:11 +0800

    布局调整。

--- src/com/example/simplenotes/MainActivity.java
+++ src/com/example/simplenotes/MainActivity.java
@@ -18,6 +18,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -32,6 +33,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
         edit = (EditText) findViewById(R.id.edit);
         
